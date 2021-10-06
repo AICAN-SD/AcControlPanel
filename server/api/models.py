d2d49e7c245213e1a58768769df90cd32ad900cb
Alankrit Arya <alankritarya15@gmail.com>
2021-10-06 11:39:57 +0530

    Models

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+
+class Floors(models.Model):
+    FloorId = models.AutoField(primary_key=True)
+    FloorName = models.CharField(max_length=200)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.FloorName
+
+class Rooms(models.Model):
+    RoomId = models.AutoField(primary_key=True)
+    floor = models.ForeignKey(Floors,on_delete=models.CASCADE)
+    RoomName = models.CharField(max_length=200)
+    status = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.RoomName
+
+class Machines(models.Model):
+    MachineId = models.AutoField(primary_key=True)
+    room = models.ForeignKey(Rooms, on_delete=models.CASCADE)
+    MachineName = models.CharField(max_length=200) 
+    status = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.MachineName  
+
+class TimeScheduleFloor(models.Model):
+    floor=models.ForeignKey(Floors, on_delete=models.CASCADE)
+    startTime = models.TimeField(auto_now=False, auto_now_add=False)
+    endTime = models.TimeField(auto_now=False, auto_now_add=False)
+
+    class Meta:
+        ordering=['startTime']
+
+class TimeScheduleRoom(models.Model):
+    room=models.ForeignKey(Rooms, on_delete=models.CASCADE)
+    startTime = models.TimeField(auto_now=False, auto_now_add=False)
+    endTime = models.TimeField(auto_now=False, auto_now_add=False)  
+
+    class Meta:
+        ordering=['startTime']  
+
+class TimeScheduleMachine(models.Model):
+    machine=models.ForeignKey(Machines, on_delete=models.CASCADE)
+    startTime = models.TimeField(auto_now=False, auto_now_add=False)
+    endTime = models.TimeField(auto_now=False, auto_now_add=False)    
+
+    class Meta:
+        ordering=['startTime']
--- server/api/views.py
+++ server/api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+from .models import Floors
 
-# Create your views here.
-def home(request):
+
+def SchedulingApi(request):
     return HttpResponse("home")
