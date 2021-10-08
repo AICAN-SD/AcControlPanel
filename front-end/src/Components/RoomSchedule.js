60809350e98afccd116131ff0e18e46cbe0d523a
Alankrit Arya <alankritarya15@gmail.com>
2021-10-08 10:46:34 +0530

    Scheduling frontend

--- front-end/src/Components/RoomSchedule.js
+++ front-end/src/Components/RoomSchedule.js
@@ -1,46 +1,72 @@
 import React, { useState, useEffect } from "react";
-import { Button } from "react-bootstrap";
-import SchedulingTable from "./SchedulingTable";
-import { datax, datay } from "../sampleData";
+import { Form, Button } from "react-bootstrap";
+import TimeField from "react-simple-timefield";
 
 function RoomSchedule() {
-  const headers = [
-    {
-      id: 1,
-      name: "Room1"
-    },
-    {
-      id: 2,
-      name: "Room2"
-    },
-    {
-      id: 3,
-      name: "Room3"
-    },
-    {
-      id: 4,
-      name: "Room4"
-    }
-  ];
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    setData(datax);
-  }, []);
-
-  function changeHandler(e) {
-    if (e.target.value === "1") {
-      setData(datax);
-    } else {
-      setData(datay);
-    }
+  const [time, setTime] = useState("12:33");
+  function onTimeChange(e) {
+    setTime(e.target.value);
   }
   return (
-    <SchedulingTable
-      data={data}
-      headers={headers}
-      changeHandler={changeHandler}
-    />
+    <div>
+      <Form>
+        <Form.Label sm="2">Profile Name:</Form.Label>
+        <Form.Control type="text" placeholder="" />
+        <h4>Choose Rooms:</h4>
+        <h5>Floor1:</h5>
+        <Form.Check type="checkbox" label="Room 1" />
+        <Form.Check type="checkbox" label="Room 2" />
+        <h5>Floor2:</h5>
+        <Form.Check type="checkbox" label="Room 1" />
+        <Form.Check type="checkbox" label="Room 2" />
+      </Form>
+      <Form.Label sm="2">Start Time:</Form.Label>
+      <TimeField
+        style={{ width: "80px", margin: "5px" }}
+        value={time}
+        onChange={onTimeChange}
+      />
+      <Form.Label sm="2">End Time:</Form.Label>
+      <TimeField
+        style={{ width: "80px", margin: "5px" }}
+        value={time}
+        onChange={onTimeChange}
+      />
+
+      <Button style={{ margin: "5px" }} variant="primary" type="submit">
+        Add Field
+      </Button>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Start Time</th>
+            <th scope="col">End Time</th>
+            <th scope="col">Running Time</th>
+            <th>#</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>startTime</td>
+            <td>endTime</td>
+            <td>hrs</td>
+            <td>edit delete</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>startTime</td>
+            <td>endTime</td>
+            <td>hrs</td>
+            <td>edit delete</td>
+          </tr>
+        </tbody>
+      </table>
+      <Button variant="primary" type="submit">
+        Create
+      </Button>
+    </div>
   );
 }
 
