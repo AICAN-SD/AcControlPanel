b3744747173c7874d2e3ab5ad02fe4d9adcbe553
Alankrit Arya <alankritarya15@gmail.com>
2021-10-09 19:32:28 +0530

    machines url fixed

--- server/api/urls.py
+++ server/api/urls.py
@@ -8,13 +8,4 @@ urlpatterns = [
    path('api/rooms/',views.Room,name="room"),#get data for creating roomProfile
 
    path('api/machines/',views.Machine,name="machine"),#get data for creating machineProfile
-
-   path('TSF1/<str:fid>/',views.TimeSF),#All Schedule records for a floor
-   path('TSF/<str:id>/',views.TimeSF),#specific floor Schedule record
-
-   path('TSR1/<str:rid>/',views.TimeSR),#All Schedule records for a Room
-   path('TSR/<str:id>/',views.TimeSR),#specific Room Schedule record
-
-   path('TSM1/<str:mid>',views.TimeSM),#All Schedule records for a Machine
-   path('TSM/<str:id>/',views.TimeSM)#specific Machine Schedule record
 ]
