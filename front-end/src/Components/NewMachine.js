6ca708a8013c4e306260adbc569f8453e904b859
Alankrit Arya <alankritarya15@gmail.com>
2021-10-15 15:01:17 +0530

    front changes

--- front-end/src/Components/NewMachine.js
+++ front-end/src/Components/NewMachine.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import {Form,Button} from "react-bootstrap"
+import axios from 'axios'
 
 function NewMachine({cpu}) {
     const [name,setName] = useState("")
@@ -9,7 +10,22 @@ function NewMachine({cpu}) {
 
    function submitHandler(e){
     e.preventDefault() 
-   }
+    const data={
+        name:name,
+        power:pw,
+        capacity:capacity,
+        costperunit:cpu
+    }
+    axios.post('http://127.0.0.1:8000/api/devices/',{data:data,headers: {
+        'Content-Type' : 'application/json; charset=UTF-8',
+        'Accept': 'Token',
+        "Access-Control-Allow-Origin": "*",
+    }
+   })
+   .then(()=>{
+       window.location.reload(false)
+   })
+}
     return (
         <div style={{paddingLeft:"40px"}}>
         <Form onSubmit={submitHandler}>
@@ -19,18 +35,21 @@ function NewMachine({cpu}) {
          onChange={(e) => setName(e.target.value)}
          type="text"
          placeholder=""
+         required
        /><br/>
        <h5>Power Rating:</h5>
         <input
         type="number"
         onChange={(e)=>setPw(e.target.value)}
         value={pw}
+        required
        /><input style={{width:"70px",textAlign:"center"}} type="text" value="Watt" disabled/><br/>
        <h5>Capacity:</h5>
         <input
         type="number"
         onChange={(e)=>setCapacity(e.target.value)}
         value={capacity}
+        required
        /><input style={{width:"70px",textAlign:"center"}} type="text" value="Ton" disabled/><br/><br/>
             <Button type="submit">Create</Button>
         </Form>
--- front-end/src/Screens/CreateMachine.js
+++ front-end/src/Screens/CreateMachine.js
@@ -11,6 +11,7 @@ function CreateMachine() {
     const [editForm, setEditForm] = useState(false)
     const [devices,setDevices] = useState([])
     const [cpu,setCpu] = useState(6.5)
+    const [mid,setMid] = useState(null)
 
     useEffect(()=>{
         const data =async ()=>await axios.get('http://127.0.0.1:8000/api/devices/')
@@ -28,10 +29,35 @@ function CreateMachine() {
     }
     function update(id){
         setEditForm(true)
+        setMid(id)
     }
     function back(){
         setEditForm(false)
     }
+    function cpuHandler(){
+       const data={
+            cost:cpu
+        }
+        axios.post('http://127.0.0.1:8000/api/cost/',{data:data,headers: {
+            'Content-Type' : 'application/json; charset=UTF-8',
+            'Accept': 'Token',
+            "Access-Control-Allow-Origin": "*",
+        }
+       })
+       .then(()=>{
+           window.location.reload(false)
+       })
+    }
+    function Delete(id){
+        const newP = devices.filter(device=>{
+            return device.deviceId!== id 
+          })
+          setDevices([...newP])
+          axios.delete(`http://127.0.0.1:8000/api/devices/${id}`)
+          .catch(e=>{
+            console.log(e)
+          })
+        }
     return (
         <div style={{paddingLeft:"40px"}}>
             <label >Cost/unit:</label> <input
@@ -40,7 +66,7 @@ function CreateMachine() {
         onChange={(e)=>setCpu(e.target.value)}
         step=".01"
         value={cpu}
-       />
+       />{"₹ "}<Button onClick={cpuHandler} size="sm">Update</Button>
             {!editForm && <>
             <Button onClick={clickHandler} style={{float:"right"}}> {form?"Back":"+ Create New"}</Button><br/><br/><br/>
             {!form && 
@@ -49,15 +75,15 @@ function CreateMachine() {
                 return <div style={{margin:"10px",padding:"20px",borderRadius:"10px",border: '1px solid black'}}>
                    <h2>{device.name}</h2> 
                    <h5 style={{display:"inline"}}>Capacity: {device.capacity} Ton</h5>
-                   <Button style={{float:"right"}} variant="clear" onClick={()=>update(1)} ><FontAwesomeIcon icon={faEdit} style={{color:"blue"}} size="lg"/></Button>
-                   <Button style={{float:"right"}} variant="clear" ><FontAwesomeIcon icon={faTrash} style={{color:"red"}} size="lg"/></Button><br/>
+                   <Button style={{float:"right"}} variant="clear" onClick={()=>update(device.deviceId)} ><FontAwesomeIcon icon={faEdit} style={{color:"blue"}} size="lg"/></Button>
+                   <Button style={{float:"right"}} variant="clear" onClick={()=>Delete(device.deviceId)} ><FontAwesomeIcon icon={faTrash} style={{color:"red"}} size="lg"/></Button><br/>
                    <h5>Power Rating: {device.powerRating} Watt</h5>
                 </div>
             })}
              </>}
              {form && <NewMachine cpu={cpu}/>} 
              </>}    
-             {editForm &&<> <Button onClick={back} style={{float:"right"}}>Cancel</Button><br/><EditMachine/></>}   
+             {editForm &&<> <Button onClick={back} style={{float:"right"}}>Cancel</Button><br/><EditMachine cpu={cpu} mid={mid}/></>}   
         </div>
     )
 }
